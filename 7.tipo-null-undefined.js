9186db3a9fb98f7e85426fc201371d96155c210a
Abominavel_Sirius <abominavelsirius@gmail.com>
2024-10-16 14:56:11 -0300

    add

---
+++ 7.tipo-null-undefined.js
@@ -0,0 +1,18 @@
+let nomeEstudante;
+const professora = 'Ana';
+
+console.log(nomeEstudante);
+console.log(typeof nomeEstudante);
+console.log(typeof professora);
+
+let telefoneEstudante = null;
+
+console.log(typeof telefoneEstudante);
+
+
+
+
+
+
+// não existir nenhum valor é diferente de valor indefinido
+// O null é muito útil, por exemplo, em situações de cadastro, quando há um campo sem nenhum valor.
